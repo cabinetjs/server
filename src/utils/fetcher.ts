1874eb2ce412c12c94d95e71e8eafd254ed131c7
Sophia <me@sophia-dev.io>
2023-06-16 08:46:17 +0900

    fix(network): now fetcher will retry requests when it failed

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -2,15 +2,18 @@ import fetch, { Response } from "node-fetch";
 
 import { StringKeyOf } from "@utils/types";
 import { buildSearchParams } from "@utils/url";
+import { Logger } from "@utils/logger";
 
 export interface FetchOptions {
     method?: "GET" | "POST";
     headers?: Record<string, string>;
     params?: Record<string, string | number>;
+    retryCount?: number;
 }
 
 export class Fetcher<TEndpointMap extends Record<string, any>> {
     private readonly baseUrl: string;
+    private readonly logger = new Logger(Fetcher.name);
 
     public constructor(baseUrl: string) {
         this.baseUrl = baseUrl;
@@ -20,33 +23,63 @@ export class Fetcher<TEndpointMap extends Record<string, any>> {
         endpoint: Endpoint,
         options?: FetchOptions,
     ): Promise<Response> {
-        const { method = "GET", headers = {}, params } = options ?? {};
-        const url = `${this.baseUrl}${endpoint}`;
-        let urlWithParams = url;
-        if (params) {
-            const newParams = { ...params };
-            for (const [key, value] of Object.entries(newParams)) {
-                if (!urlWithParams.includes(`{${key}}`)) {
-                    continue;
+        const { method = "GET", headers = {}, params, retryCount = 5 } = options ?? {};
+
+        try {
+            const url = `${this.baseUrl}${endpoint}`;
+            let urlWithParams = url;
+            if (params) {
+                const newParams = { ...params };
+                for (const [key, value] of Object.entries(newParams)) {
+                    if (!urlWithParams.includes(`{${key}}`)) {
+                        continue;
+                    }
+
+                    urlWithParams = urlWithParams.replace(new RegExp(`\{${key}\}`, "g"), value.toString());
+                    delete newParams[key];
                 }
 
-                urlWithParams = urlWithParams.replace(new RegExp(`\{${key}\}`, "g"), value.toString());
-                delete newParams[key];
+                urlWithParams += `?${buildSearchParams(params)}`;
             }
 
-            urlWithParams += `?${buildSearchParams(params)}`;
+            const response = await fetch(urlWithParams, {
+                method,
+                headers,
+            });
+
+            if (!response.ok) {
+                throw new Error(`Failed to fetch data from ${url}. (${response.status} ${response.statusText})`);
+            }
+
+            return response;
+        } catch (error) {
+            let errorMessage: string;
+            let stack: string | undefined;
+            if (error instanceof Error) {
+                errorMessage = error.message;
+                stack = error.stack;
+            } else {
+                errorMessage = `${error}`;
+            }
+
+            if (retryCount > 0) {
+                this.logger.warn(`Failed to fetch data from {cyan}: {red}`, undefined, `'${endpoint}'`, errorMessage);
+                this.logger.warn("Remaining retry count: {cyan}", undefined, retryCount);
+
+                return this.fetch(endpoint, {
+                    ...options,
+                    method,
+                    headers,
+                    params,
+                    retryCount: retryCount - 1,
+                });
+            }
+
+            this.logger.error("Failed to fetch data from {cyan}.", undefined, undefined, `'${endpoint}'`);
+            this.logger.error("Error: {red}", stack, undefined, errorMessage);
+
+            throw error;
         }
-
-        const response = await fetch(urlWithParams, {
-            method,
-            headers,
-        });
-
-        if (!response.ok) {
-            throw new Error(`Failed to fetch data from ${url}. (${response.status} ${response.statusText})`);
-        }
-
-        return response;
     }
     public async fetchJson<Endpoint extends StringKeyOf<TEndpointMap>>(
         endpoint: Endpoint,
